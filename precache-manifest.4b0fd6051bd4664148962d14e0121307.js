c63f903257ea61be50db1a5673e8dc1b395dff5f
umeranjum17 <umeranjum17@gmail.com>
2020-12-14 21:58:04 +0400

    Updates

---
+++ precache-manifest.4b0fd6051bd4664148962d14e0121307.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d426aff7946a92a4d8a91a05cb502f0",
+    "url": "/PortfolioV2/index.html"
+  },
+  {
+    "revision": "4cd88e02c83773bce6b9",
+    "url": "/PortfolioV2/static/css/main.ed94be71.chunk.css"
+  },
+  {
+    "revision": "725fb7ee4d941fa3d0d2",
+    "url": "/PortfolioV2/static/js/2.2420a935.chunk.js"
+  },
+  {
+    "revision": "4cd88e02c83773bce6b9",
+    "url": "/PortfolioV2/static/js/main.8826737c.chunk.js"
+  },
+  {
+    "revision": "5efe74379d1fe97c3fc3",
+    "url": "/PortfolioV2/static/js/runtime~main.15178de7.js"
+  },
+  {
+    "revision": "9093453b5c76cfa096f36d829d7c43aa",
+    "url": "/PortfolioV2/static/media/af.9093453b.PNG"
+  },
+  {
+    "revision": "b25f32580b6a2de280bb5485c190af15",
+    "url": "/PortfolioV2/static/media/gf.b25f3258.PNG"
+  },
+  {
+    "revision": "cec5d7f0578de9cc774c10ce8e630b6b",
+    "url": "/PortfolioV2/static/media/linkedin.cec5d7f0.PNG"
+  },
+  {
+    "revision": "f910ebed5102c12b5d89f7427c9ec825",
+    "url": "/PortfolioV2/static/media/me.f910ebed.gif"
+  },
+  {
+    "revision": "8e7271b8d91a86aa2fdfb45c0bd3dcd0",
+    "url": "/PortfolioV2/static/media/mp.8e7271b8.PNG"
+  },
+  {
+    "revision": "48452b5060d8a55e49c67c6946a8bd29",
+    "url": "/PortfolioV2/static/media/pn.48452b50.PNG"
+  },
+  {
+    "revision": "42c27db6820bad46cd9e3aca7a6e5a61",
+    "url": "/PortfolioV2/static/media/resume.42c27db6.pdf"
+  },
+  {
+    "revision": "9f748063470f4e21b7ffaa89915d6fda",
+    "url": "/PortfolioV2/static/media/sq.9f748063.PNG"
+  },
+  {
+    "revision": "b65a2a391f265e993d67fea1e5a4f2bf",
+    "url": "/PortfolioV2/static/media/wy.b65a2a39.PNG"
+  }
+]);
