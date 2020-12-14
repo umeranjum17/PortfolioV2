7d8a52d46396c73ebed6b7a5c6409b2326e41a11
umeranjum17 <umeranjum17@gmail.com>
2020-12-14 23:50:19 +0400

    Changes in netlfy toml and href additions

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ const Card = ({project:{title,description,image}}) => {
     console.log(image)
     return (
         <div className=" m-4 h-72 w-72  shadow-md rounded-md">
-           <img   src={require('../assets/'+image)} ></img> 
+           <img alt="image-project"  src={require('../assets/'+image)} ></img> 
            <h3 className="p-3 text-xl font-semibold">
               {title}
            </h3>
--- src/components/FindMe.jsx
+++ src/components/FindMe.jsx
@@ -9,7 +9,7 @@ const FindMe = () => {
             <h1 className="font-black text-xl md:text-3xl my-10 text-center">Find Me on LinkedIn & Github </h1>
             <ul className="flex items-center justify-center text-2xl text-white">
                 <li><a href="https://www.linkedin.com/in/umer-farooq-ab8315131/" className="social-icon"> <i className="fa text-white mx-2 bg-blue-800 fa-linkedin"></i></a></li>
-                <li><a href="" className="social-icon"> <i className="fa text-white mx-2  bg-gray-800 fa-github"></i></a></li>
+                <li><a href="https://github.com/umeranjum17" className="social-icon"> <i className="fa text-white mx-2  bg-gray-800 fa-github"></i></a></li>
             </ul>
             <div className="font-black text-link text-center mt-10">
                 <a href={pdf} target="_blank"> Resume.pdf </a>
