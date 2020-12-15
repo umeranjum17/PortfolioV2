d357365af75dfa1e08045e63548c9197998a6bf8
umeranjum17 <umeranjum17@gmail.com>
2020-12-15 22:31:04 +0400

    serverless integration progress

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,25 +1,39 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Axios } from '../firebase/firebaseConfig'
 const Contact = () => {
+  const [form, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    msg: ""
+
+  })
+  const submitForm = (e) => {
+    e.preventDefault()
+    Axios.post("https://us-central1-notification-ceb6a.cloudfunctions.net/email",form)
+  }
+  const handleChange = (e) => {
+    setFormData({ ...form, [e.target.name]: e.target.value })
+  }
   return (
     <>
       <h1 className="font-black text-3xl my-10 text-center">
         Drop Me a mail I would Love to talk to you....
         </h1>
-      <form className="w-full max-w-lg mx-auto">
+      <form className="w-full max-w-lg mx-auto" onSubmit={submitForm}>
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
               First Name
       </label>
-            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" />
-            <p className="text-red-500 text-xs italic">Please fill out this field.</p>
+            <input onChange={handleChange} name="firstName" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" />
+
           </div>
           <div className="w-full md:w-1/2 px-3">
             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
               Last Name
       </label>
-            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" />
+            <input onChange={handleChange} name="lastName" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" />
           </div>
         </div>
         <div className="flex flex-wrap -mx-3 mb-6">
@@ -27,22 +41,21 @@ const Contact = () => {
             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
               E-mail
       </label>
-            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" />
-            <p className="text-gray-600 text-xs italic">Some tips - as long as needed</p>
+            <input onChange={handleChange} name="email" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" />
           </div>
         </div>
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full px-3">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+            <label  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
               Message
+
       </label>
-            <textarea className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message"></textarea>
-            <p className="text-gray-600 text-xs italic">Re-size can be disabled by set by resize-none / resize-y / resize-x / resize</p>
+<textarea onChange={handleChange} name="msg" cols="30" rows="10"  className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" ></textarea>
           </div>
         </div>
         <div className="md:flex md:items-center">
           <div className="md:w-1/3">
-            <button className="shadow bg--400 hover:bg--400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+            <button className="shadow  focus:shadow-outline bg-black focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
               Send
       </button>
           </div>
